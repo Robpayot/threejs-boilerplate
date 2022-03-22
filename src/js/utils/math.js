d680e2e0b96dc7187e20978ce95feb8e5c3a25be
Robin payot <robpayot@gmail.com>
2022-03-22 11:27:06 +0100

    add lerp in utils

--- src/js/utils/math.js
+++ src/js/utils/math.js
@@ -11,11 +11,22 @@ export function clamp(value, min, max) {
 }
 
 export function toRadian(degrees) {
-  return degrees * Math.PI / 180
+  return (degrees * Math.PI) / 180
 }
 
 export function oscillateBetween(time, min, max, frequence = 1, offset = 0) {
   const amplitude = max - min
   const average = amplitude / 2 + min
-  return Math.sin(time * frequence + offset) * amplitude / 2 + average
+  return (Math.sin(time * frequence + offset) * amplitude) / 2 + average
+}
+
+/**
+ * Linear interpolation function
+ * must match primary colors available as css vars
+ * @param start first value of the two numbers.
+ * @param end second value of the two numbers.
+ * @param amt amount by which a number is to be interpolated between the two numbers.
+ */
+export function lerp(start, end, amt) {
+  return (1 - amt) * start + amt * end
 }
